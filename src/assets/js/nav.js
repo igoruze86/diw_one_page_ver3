d60122f5999911de67600e9b0fee378861b1086c
Igor Uzelac <igor.uzelac@diwanee.com>
2018-10-18 14:00:37 +0200

    Commit-mobile and tablet design finished

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,4 +1,33 @@
 //Drop down nav menu
 $('.c-icons-burger').click(function(){
   $('.c-nav-menu-mob').slideToggle(220);
+  $(this).toggleClass('c-icons-burger-close');
+});
+
+// Hide nav menu on scroll
+var prevScroll = window.pageYOffset;
+var iconClose = $('.c-icons-burger');
+window.onscroll = function() {
+  var currScroll = window.pageYOffset;
+    if (prevScroll < currScroll) {
+      $('.c-nav-menu-mob').slideUp(220);
+      iconClose.removeClass('c-icons-burger-close');
+    }
+}
+
+// logo animation
+$(document).ready(function(){
+  $(window).scroll(function(event){
+    var y = $(this).scrollTop();
+    var logo = $('.c-icons-logo');
+    var icon = $('.c-icons-logo-icon');
+      if (y >= 60) {
+        $('.c-icons-logo-icon').addClass('show');
+        $('.c-icons-logo').addClass('hide');
+      }
+      else if (y < 60 && logo.hasClass('hide') && icon.hasClass('show')) {
+        $('.c-icons-logo').removeClass('hide');
+        $('.c-icons-logo-icon').removeClass('show');
+      }
+  });
 });
